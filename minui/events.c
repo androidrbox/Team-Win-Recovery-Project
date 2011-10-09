97229e02a5c52d3e9254d214f349786d7a4e7652
androidrbox <androidrbox@gmail.com>
2011-10-09 15:17:00 -0700

    Generate a repeated key press when keys are held down to allow continous scrolling.

--- minui/events.c
+++ minui/events.c
@@ -37,6 +37,9 @@
 #define ABS_MT_TOUCH_MAJOR 0x30
 #define SYN_MT_REPORT 2
 
+// The amount of time in ms to delay before duplicating a held down key.
+#define KEYHOLD_DELAY 200
+
 enum {
     DOWN_NOT,
     DOWN_SENT,
@@ -338,13 +341,15 @@ static int vk_modify(struct ev *e, struct input_event *ev)
     return 1;
 }
 
-int ev_get(struct input_event *ev, unsigned dont_wait)
+int ev_get(struct input_event *ev, unsigned dont_wait, unsigned keyheld)
 {
     int r;
     unsigned n;
 
     do {
-        r = poll(ev_fds, ev_count, dont_wait ? 0 : -1);
+        // When keyheld is true, that means the previous event
+        // was an up/down keypress so wait KEYHOLD_DELAY.
+        r = poll(ev_fds, ev_count, dont_wait ? 0 : keyheld ? KEYHOLD_DELAY : -1);
 
         if(r > 0) {
             for(n = 0; n < ev_count; n++) {
@@ -356,6 +361,10 @@ int ev_get(struct input_event *ev, unsigned dont_wait)
                     }
                 }
             }
+        } else if (r == 0 && keyheld) {
+            // If a timeout occurred when keyheld was set, let the
+            // caller know so it can generate a repeated event.
+            return 1;
         }
     } while(dont_wait == 0);
 
